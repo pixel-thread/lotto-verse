41aa9a39b680dc52b0ccaabe080b5353791d55f0
JyrwaJR <jyrwaboys@gmail.com>
2025-11-14 08:45:25 +0530

    veiew draw details

---
+++ frontend/src/app/draw/[id]/index.tsx
@@ -0,0 +1,7 @@
+import { DrawDetailScreen } from '@/src/components/screen/draw/DrawDetailScreen';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function page() {
+  const { id } = useLocalSearchParams();
+  return <DrawDetailScreen />;
+}
--- frontend/src/components/common/AuthRedirect.tsx
+++ frontend/src/components/common/AuthRedirect.tsx
@@ -3,22 +3,11 @@ import { Stack, usePathname, useRouter } from 'expo-router';
 import { LoadingScreen } from './LoadingScreen';
 import { useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useTheme, View } from 'tamagui';
+import { View } from 'tamagui';
 import { cn } from '@/src/lib/utils';
-import { logger } from '@/src/utils/logger';
-type HSLA = {
-  h: number;
-  s: number;
-  l: number;
-  a?: number;
-};
-const hslToCss = ({ h, s, l, a }: HSLA) => {
-  return a !== undefined ? `hsla(${h}, ${s}%, ${l}%, ${a})` : `hsl(${h}, ${s}%, ${l}%)`;
-};
 
 export const AuthRedirect = () => {
   const { isSignedIn, isLoaded } = useAuth();
-  const theme = useTheme();
   const router = useRouter();
   const pathName = usePathname();
 
@@ -32,11 +21,8 @@ export const AuthRedirect = () => {
 
   if (!isLoaded) return <LoadingScreen />;
 
-  logger.log(theme.background.variable);
-  logger.log(theme.background.val);
-  logger.log(theme.background);
   return (
-    <View flex={1} bg={'$background'}>
+    <View flex={1}>
       <SafeAreaView className={cn('flex-1')}>
         <Stack screenOptions={{ headerShown: false }} />
       </SafeAreaView>
