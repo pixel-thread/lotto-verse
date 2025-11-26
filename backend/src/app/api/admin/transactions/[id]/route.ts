a670172ae818c4ed073d05a81e8236f5f23111f3
JyrwaJR <jyrwaboys@gmail.com>
2025-11-26 09:59:31 +0530

    comment code which is not needed

--- backend/src/app/api/admin/transactions/[id]/route.ts
+++ backend/src/app/api/admin/transactions/[id]/route.ts
@@ -6,22 +6,22 @@ import { requireSuperAdmin } from "@/src/utils/middleware/requireSuperAdmin";
 import { ErrorResponse, SuccessResponse } from "@/src/utils/next-response";
 import { NextRequest } from "next/server";
 
-export const mockTransactionDetail = {
-  id: "tx_1001",
-  amount: 250,
-  status: "SUCCESS",
-  paymentMethod: "razorpay",
-  createdAt: "2025-01-10T14:20:00.000Z",
-  updatedAt: "2025-01-10T14:25:00.000Z",
-  name: "John Carter",
-  userId: "user_001",
-  imageUrl: "https://i.pravatar.cc/300?img=12",
-  purchaseId: "purchase_5001",
-  number: 42,
-  drawId: "draw_2023_07",
-  month: "2023-07",
-  entryFee: 250,
-};
+// const mockTransactionDetail = {
+//   id: "tx_1001",
+//   amount: 250,
+//   status: "SUCCESS",
+//   paymentMethod: "razorpay",
+//   createdAt: "2025-01-10T14:20:00.000Z",
+//   updatedAt: "2025-01-10T14:25:00.000Z",
+//   name: "John Carter",
+//   userId: "user_001",
+//   imageUrl: "https://i.pravatar.cc/300?img=12",
+//   purchaseId: "purchase_5001",
+//   number: 42,
+//   drawId: "draw_2023_07",
+//   month: "2023-07",
+//   entryFee: 250,
+// };
 
 export async function GET(
   req: NextRequest,
