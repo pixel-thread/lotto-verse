dc725dae9b0a7c04b24e843adf833e1b5b676691
JyrwaJR <jyrwaboys@gmail.com>
2025-11-27 11:38:51 +0530

    update

---
+++ backend/p.config.ts
@@ -0,0 +1,15 @@
+// import { defineConfig, env } from "prisma/config";
+// import "dotenv/config";
+
+// export default defineConfig({
+// schema: "./src/lib/db/prisma/schema.prisma",
+// engine: "classic",
+// migrations: {
+//   // path: "prisma/migrations",
+//   // seed: "tsx prisma/seed.ts",
+// },
+// datasource: {
+//   url: env("DATABASE_URL"),
+//   directUrl: env("DIRECT_URL"),
+// },
+// });
