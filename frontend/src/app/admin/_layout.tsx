6cfab7ef54bb28e833add1e0c5fe35ada0762875
JyrwaJR <jyrwaboys@gmail.com>
2026-01-13 20:04:21 +0530

    allow admin user

--- frontend/src/app/admin/_layout.tsx
+++ frontend/src/app/admin/_layout.tsx
@@ -3,7 +3,9 @@ import { logger } from '@/src/utils/logger';
 import { Redirect, Stack } from 'expo-router';
 
 export default function page() {
-  const { isSuperAdmin, user } = useAuth();
+  const { user } = useAuth();
+
+  const isSuperAdmin = user?.role === 'SUPER_ADMIN' || user?.role === 'ADMIN';
 
   if (!isSuperAdmin) {
     logger.info('Permission denied: Not super admin', { user: user });
