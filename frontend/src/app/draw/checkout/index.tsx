e8676744be29c613b07f1519837017fe59d9589f
JyrwaJR <jyrwaboys@gmail.com>
2026-01-16 19:10:30 +0530

    fix mark paid is not working

--- frontend/src/app/draw/checkout/index.tsx
+++ frontend/src/app/draw/checkout/index.tsx
@@ -1,13 +1,20 @@
 import { CustomHeader } from '@/src/components/common/CustomHeader';
 import { CheckoutPage } from '@/src/components/screen/checkout';
+import { useAuth } from '@/src/hooks/auth/useAuth';
+import { logger } from '@/src/utils/logger';
 import { Redirect } from 'expo-router';
 import { useSearchParams } from 'expo-router/build/hooks';
 
 export default function Checkout() {
+  const { user } = useAuth();
   const searchParams = useSearchParams();
   const id = searchParams.get('numberId');
 
-  if (!id) {
+  if (!id || id === null) {
+    logger.error('No numberId provided', {
+      userId: user?.id,
+      numberId: id,
+    });
     return <Redirect href="/" />;
   }
 
--- frontend/src/lib/endpoints/admin/winner/index.ts
+++ frontend/src/lib/endpoints/admin/winner/index.ts
@@ -4,5 +4,5 @@ type WinnerEndpoints = 'GET_ALL_WINNER' | 'PUT_MARK_PAID';
 
 export const ADMIN_WINNER_ENDPOINTS: EndpointT<WinnerEndpoints> = {
   GET_ALL_WINNER: '/admin/winners',
-  PUT_MARK_PAID: '/admin/winner/:id/mark-paid',
+  PUT_MARK_PAID: '/admin/winners/mark-paid',
 };
