5b1299b5d55613ed7739a7a9ab9dd322ee7a28ca
JyrwaJR <jyrwaboys@gmail.com>
2026-01-17 15:05:09 +0530

    remove standalone as frontend is present

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  // Add the packages in transpilePackages
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
